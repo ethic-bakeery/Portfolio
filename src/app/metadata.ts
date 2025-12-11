b97437dd816c44986fb1372fa0967800276cbc1a
Abubakar Usman <98848465+ethic-bakeery@users.noreply.github.com>
2025-12-11 11:09:26 +0530

    Remove Google verification from metadata
    Removed Google verification from metadata.

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -3,7 +3,4 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Bakeery | Security Analyst",
   description: "Professional portfolio of Abubakar Usman - Security Analyst",
-  verification: {
-    google: "xYSxjHrMIji_BHbfu7bgB9YHNM6k5b9_fMshKJA39rw",
-  },
 };
